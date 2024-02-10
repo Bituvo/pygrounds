120e7459ca2cad0f4288a3096942c4a78679bd05
Bradley Pierce <bituvocoding@gmail.com>
2024-02-10 08:42:47 -0500

    Add some comments

--- utils.py
+++ utils.py
@@ -1,11 +1,14 @@
 from datetime import datetime, timedelta
 
+# Parse Newgrounds-style date/time string
 def parse_time(date_string, time_string):
     datetime_string = f"{date_string} {time_string[:-4]}"
 
     date_object = datetime.strptime(datetime_string, "%b %d, %Y %I:%M %p")
+    # All Newgrounds times are in EST, convert to UTC
     return date_object - timedelta(hours=5)
 
+# Parse human-readable duration ("x min x sec") into seconds
 def parse_duration(duration):
     duration = duration.split()
 
